63224f4c2197842c0c48c32e9b2fdf0b7f2bab12
uscinavicius <uscinaviciusd@gmail.com>
2022-12-01 22:36:12 -0600

    i did a next.js

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = "Darius Uscinavicius";
-export const siteTitle = "Next.Js Simple Website";
+const name = 'Darius Uscinavicius';
+export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,18 +14,18 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learning how to build a personal website using next.js"
+          content="Learn how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
+            siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <headder className={styles.headder}>
+      <header className={styles.header}>
         {home ? (
           <>
             <Image
@@ -57,7 +57,7 @@ export default function Layout({ children, home }) {
             </h2>
           </>
         )}
-      </headder>
+      </header>
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
